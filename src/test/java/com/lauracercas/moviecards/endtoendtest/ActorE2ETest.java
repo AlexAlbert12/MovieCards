cb48df9e244dd9f51bbc30c08b7ed2478958e071
AlexAlbert12 <danualbert12@gmail.com>
2026-02-07 19:20:11 +0100

    Arreglar error de test

--- src/test/java/com/lauracercas/moviecards/endtoendtest/ActorE2ETest.java
+++ src/test/java/com/lauracercas/moviecards/endtoendtest/ActorE2ETest.java
@@ -74,9 +74,7 @@ public class ActorE2ETest {
         assertEquals("Nombre", headerRow.findElements(By.tagName("th")).get(1).getText());
         assertEquals("Fecha Nacimiento", headerRow.findElements(By.tagName("th")).get(2).getText());
         assertEquals("Pais", headerRow.findElements(By.tagName("th")).get(3).getText());
-
         assertEquals("Fallecimiento", headerRow.findElements(By.tagName("th")).get(4).getText());
-        assertEquals("Editar", headerRow.findElements(By.tagName("th")).get(5).getText());
+        assertEquals("Editar", headerRow.findElements(By.tagName("th")).get(5).getText()); 
     }
-
 }
